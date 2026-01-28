4e32a369c5be29f5c921da5726cee2af71d11ca3
sachin7con <sachin7con@gmail.com>
2026-01-28 17:53:48 +0530

    Completed assignment functions

--- script.js
+++ script.js
@@ -7,21 +7,51 @@ let arr = [
 ];
 
 function PrintDeveloperbyMap() {
-  //Write your code here , just console.log
+  //SGN Write your code here , just console.log
+  arr.map((item, index)=> {
+    if(item.profession === "developer"){
+      console.log(item);
+    }
+    
+  })
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((item) => {
+    if(item.profession === "developer"){
+      console.log(item);
+    }
+})
 }
 
 function addData() {
   //Write your code here, just console.log
+  const newEmployee = {
+    id:4,
+    name: "susan",
+    age: "20",
+    profession: "intern"
+  }
+  arr.push(newEmployee);
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  arr = arr.filter((item) => {
+    return item.profession !== "admin";
+  })
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const newArr = [
+    { id: 5, name: "mike", age: "22", profession: "developer" },
+    { id: 6, name: "anna", age: "21", profession: "intern" },
+    { id: 7, name: "robert", age: "23", profession: "admin" },
+  ];
+  const result = arr.concat(newArr);
+  console.log(result);
 }
